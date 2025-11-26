9f295f2318383174fc24989984c912e592c4a21e
Ko JaeGun <kogun0626@naver.com>
2025-11-26 19:39:10 +0900

    feat: #86 db연동 및 마이그레이션 구현 (#87)

---
+++ version02/quiz09/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = 'question'
+
+    id = Column(Integer, primary_key=True, index=True)
+    subject = Column(String(200), nullable=False)
+    content = Column(String, nullable=False)
+    create_date = Column(DateTime, default=datetime.utcnow)
+
+
